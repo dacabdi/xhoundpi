07173827503394092ad511ddc2e4e195058d77cd
David Cabrera <dacabrer@microsoft.com>
2021-02-19 18:26:45 -0800

    feature/module execution entry point + configuration parser (#4)
    1. Create module execution entry point
    2. Add a configuration parser that consumes options from command line, environment variables, and configuration files (in decreasing override order).
    3. Fix test discoverability in VSCode
    4. Some other minor fixes

---
+++ xhoundpi/config.py
@@ -0,0 +1,14 @@
+"""Configuration parser and provider"""
+
+import configargparse
+
+def setup_configparser() -> configargparse.ArgumentParser:
+    """Sets up the configuration parser, provides command usage, etc."""
+    parser = configargparse.Parser(
+        auto_env_var_prefix="XHOUNDPI_",
+        add_config_file_help=False,)
+    parser.add('--config', metavar='FILE', default='xhoundpi.conf', dest='config',
+        is_config_file=True, env_var="XHOUNDPI_CONFIG", help='config file path')
+    parser.add('--log', default='xhoundpi.log', dest='log', metavar='FILE', help='logs file path')
+    parser.add('--verbose', dest='verbose', action='store_true', help='verbose logging execution')
+    return parser
