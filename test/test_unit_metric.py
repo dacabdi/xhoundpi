27dcae9825495f7d474fa9c681ae06882ef687fe
David Cabrera <dacabdi@gmail.com>
2021-03-20 15:31:56 -0700

    created time abstractions, latency and counter metrics, tested them, and did general cleanups

---
+++ test/test_unit_metric.py
@@ -0,0 +1,129 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+import unittest
+from unittest.mock import Mock, call
+from xhoundpi.metric import (LatencyMetric,
+                            CounterMetric,
+                            SuccessCounterMetric)
+
+from .fake_stop_watch import FakeStopWatch
+
+class test_LatencyMetric(unittest.TestCase): # pylint: disable=invalid-name
+
+    def test_latency(self):
+        stopwatch = FakeStopWatch()
+        hook = Mock()
+        metric = LatencyMetric('latency1', stopwatch, [hook])
+
+        metric.start()
+        self.assertEqual(metric.value, float('inf'))
+        stopwatch.elapsed = 10.0
+        metric.stop()
+
+        self.assertEqual(metric.dimension, 'latency1')
+        self.assertEqual(metric.value, 10.0)
+        hook.assert_called_once_with('latency1', 10.0)
+
+        metric.start()
+        self.assertEqual(metric.value, float('inf'))
+        stopwatch.elapsed = 20.0
+        metric.stop()
+
+        self.assertEqual(metric.dimension, 'latency1')
+        self.assertEqual(metric.value, 20.0)
+        hook.assert_called_with('latency1', 20.0)
+
+    def test_latency_as_context_manager(self):
+        stopwatch = FakeStopWatch()
+        hook = Mock()
+        metric = LatencyMetric('latency1', stopwatch, [hook])
+
+        self.assertEqual(metric.value, float('inf'))
+
+        with metric:
+            self.assertEqual(metric.value, float('inf'))
+            stopwatch.elapsed = 10.0
+
+        self.assertEqual(metric.dimension, 'latency1')
+        self.assertEqual(metric.value, 10.0)
+        hook.assert_called_once_with('latency1', 10.0)
+
+        with metric:
+            self.assertEqual(metric.value, float('inf'))
+            stopwatch.elapsed = 20.0
+
+        self.assertEqual(metric.dimension, 'latency1')
+        self.assertEqual(metric.value, 20.0)
+        hook.assert_called_with('latency1', 20.0)
+
+    def test_latency_as_context_manager_bubbles_exception(self):
+        stopwatch = FakeStopWatch()
+        hook = Mock()
+        metric = LatencyMetric('latency1', stopwatch, [hook])
+
+        self.assertEqual(metric.value, float('inf'))
+
+        with metric:
+            self.assertEqual(metric.value, float('inf'))
+            stopwatch.elapsed = 10.0
+
+        self.assertEqual(metric.dimension, 'latency1')
+        self.assertEqual(metric.value, 10.0)
+        hook.assert_called_once_with('latency1', 10.0)
+
+        with metric:
+            self.assertEqual(metric.value, float('inf'))
+            stopwatch.elapsed = 20.0
+
+        self.assertEqual(metric.dimension, 'latency1')
+        self.assertEqual(metric.value, 20.0)
+        hook.assert_called_with('latency1', 20.0)
+
+class test_CounterMetric(unittest.TestCase): # pylint: disable=invalid-name
+
+    def test_counter(self):
+        hook = Mock()
+        metric = CounterMetric('counter1', [hook])
+        # NOTE counters notify the hook with initial
+        # value to ensure priming metric collectors
+        hook.assert_called_once_with('counter1', 0)
+        self.assertEqual(metric.value, 0)
+        metric.increase()
+        hook.assert_called_with('counter1', 1)
+        self.assertEqual(metric.value, 1)
+        metric.increase()
+        hook.assert_called_with('counter1', 2)
+        self.assertEqual(metric.value, 2)
+
+class test_SuccessCounterMetric(unittest.TestCase): # pylint: disable=invalid-name
+
+    def test_counter(self):
+        hook = Mock()
+        metric = SuccessCounterMetric('counter1', [hook])
+
+        # NOTE counters notify the hook with initial
+        # value to ensure priming metric collectors
+        hook.assert_has_calls([call('counter1_Success', 0), call('counter1_Failure', 0)])
+        self.assertEqual(metric.success, 0)
+        self.assertEqual(metric.failure, 0)
+        self.assertEqual(metric.total, 0)
+
+        metric.increase(is_success=True)
+        hook.assert_called_with('counter1_Success', 1)
+        self.assertEqual(metric.success, 1)
+        self.assertEqual(metric.failure, 0)
+        self.assertEqual(metric.total, 1)
+
+        metric.increase(is_success=False)
+        hook.assert_called_with('counter1_Failure', 1)
+        self.assertEqual(metric.success, 1)
+        self.assertEqual(metric.failure, 1)
+        self.assertEqual(metric.total, 2)
+
+        metric.increase(is_success=True)
+        hook.assert_called_with('counter1_Success', 2)
+        self.assertEqual(metric.success, 2)
+        self.assertEqual(metric.failure, 1)
+        self.assertEqual(metric.total, 3)
---
+++ xhoundpi/metric.py
@@ -0,0 +1,124 @@
+""" Metrics abstractions module """
+
+import collections
+from typing import Any, Callable, Iterable, List, Mapping
+
+from .time import IStopWatch
+
+class MetricBase:
+    """ Metrics base class """
+
+    def __init__(self, dimension: str, hooks = List[Callable]):
+        self._value = None
+        self.__dimension = dimension
+        self.__hooks = hooks
+
+    @property
+    def dimension(self) -> str:
+        """ Get dimension tag """
+        return self.__dimension
+
+    @property
+    def value(self) -> Any:
+        """ Get metric value """
+        return self._value
+
+    def _call_hooks(self):
+        if isinstance(self.value, collections.Mapping):
+            self._call_hooks_multidim(self.value)
+            return
+        self._call_hooks_single_dim(self.dimension, self.value)
+
+    def _call_hooks_single_dim(self, dimension: str, value: Any):
+        for hook in self.__hooks:
+            hook(dimension, value)
+
+    def _call_hooks_multidim(self, mapping: Mapping):
+        for key, value in mapping.items():
+            self._call_hooks_single_dim(key, value)
+
+class LatencyMetric(MetricBase):
+    """ Operation latency metric with context manager semantics """
+
+    def __init__(self, dimension: str, stopwatch: IStopWatch, hooks = List[Callable]):
+        super().__init__(dimension, hooks)
+        self.__stopwatch = stopwatch
+        self._value = float('inf')
+
+    def start(self):
+        """ Start timer """
+        self._value = float('inf')
+        self.__stopwatch.start()
+
+    def stop(self):
+        """ Stop timer """
+        self._value, _ = self.__stopwatch.stop()
+        self._call_hooks()
+
+    def __enter__(self):
+        """Start a new timer as a context manager"""
+        self.start()
+        return self
+
+    def __exit__(self, *exc_info):
+        """Stop the context manager timer"""
+        self.stop()
+        return exc_info is None
+
+class CounterMetric(MetricBase):
+    """ Operation counter metric """
+
+    def __init__(self, dimension: str, hooks = List[Callable]):
+        super().__init__(dimension, hooks)
+        self._value = 0
+        self._call_hooks()
+
+    def increase(self):
+        """ Increase internal counter value """
+        self._value += 1
+        self._call_hooks()
+        return self.value
+
+class SuccessCounterMetric(MetricBase):
+    """ Success/failure operation counter metric """
+
+    SUCCESS_SUFFIX='Success'
+    FAILURE_SUFFIX='Failure'
+
+    def __init__(self, dimension: str, hooks = List[Callable]):
+        super().__init__(dimension, hooks)
+        self._value = {
+            self._compose_dimension(is_success=True): 0,
+            self._compose_dimension(is_success=False): 0,
+        }
+        self._call_hooks()
+
+    def increase(self, is_success: bool):
+        """ Increase internal counter value for success/failure """
+        self._increase(is_success=is_success)
+        return self.value
+
+    @property
+    def success(self):
+        """ Report success counter """
+        return self._value[self._compose_dimension(is_success=True)]
+
+    @property
+    def failure(self):
+        """ Report failure counter """
+        return self._value[self._compose_dimension(is_success=False)]
+
+    @property
+    def total(self):
+        """ Report total counter """
+        return self.success + self.failure
+
+    def _compose_dimension(self, is_success: bool):
+        suffix = self.__class__.SUCCESS_SUFFIX if is_success else self.__class__.FAILURE_SUFFIX
+        return f'{self.dimension}_{suffix}'
+
+    def _increase(self, is_success: bool):
+        dimension = self._compose_dimension(is_success)
+        self._value[dimension] += 1
+        self._call_hooks_single_dim(dimension, self._value[dimension])
+        return self.value
