c4f81bb63151689572c6683d171811affc2d85d9
David Cabrera <dacabdi@gmial.com>
2021-04-25 12:32:37 -0700

    fix multiple event bus subscriber problem by using subjects

--- test/test_unit_event_bus.py
+++ test/test_unit_event_bus.py
@@ -25,6 +25,32 @@ class test_EventBus(unittest.TestCase):
         run_sync(queue.put("second"))
         subscriber.assert_called_with("second")
         disposable.dispose()
+        run_sync(queue.put("third"))
+        subscriber.assert_called_with("second") # the last call after disposal is "second"
+
+    def test_from_queue_multiple_subscribers(self): # pylint: disable=no-self-use
+        subscriber1 = Mock()
+        subscriber2 = Mock()
+        subscriber3 = Mock()
+        queue = asyncio.queues.Queue()
+        event_bus = EventBus.from_async_queue(queue)
+        disposable1 = event_bus.subscribe(on_next=subscriber1)
+        disposable2 = event_bus.subscribe(on_next=subscriber2)
+        disposable3 = event_bus.subscribe(on_next=subscriber3)
+        subscriber1.assert_not_called()
+        subscriber2.assert_not_called()
+        subscriber3.assert_not_called()
+        run_sync(queue.put("first"))
+        subscriber1.assert_called_once_with("first")
+        subscriber2.assert_called_once_with("first")
+        subscriber3.assert_called_once_with("first")
+        run_sync(queue.put("second"))
+        subscriber1.assert_called_with("second")
+        subscriber2.assert_called_with("second")
+        subscriber3.assert_called_with("second")
+        disposable1.dispose()
+        disposable2.dispose()
+        disposable3.dispose()
 
     def test_exception(self): # pylint: disable=no-self-use
         on_next = Mock()
--- xhoundpi/event_bus.py
+++ xhoundpi/event_bus.py
@@ -4,6 +4,7 @@ import asyncio
 import functools
 
 import rx
+from rx.subject.subject import Subject
 from rx.disposable import Disposable
 
 from .queue_decorators import with_callback # pylint: disable=unused-import
@@ -14,14 +15,16 @@ class EventBus:
 
     # from https://blog.oakbits.com/rxpy-and-asyncio.html
     @staticmethod
-    def from_async_queue(queue, loop=None):
+    def from_async_queue(queue, loop = None) -> rx.core.typing.Observable :
         """ Creates an observable from dequeueing an async queue """
         # pylint: disable=broad-except
         if loop is None:
             loop = asyncio.get_event_loop()
+
         def on_subscribe(observer, scheduler):
             # pylint: disable=unused-argument
             queue_with_callback = queue.with_callback(observer.on_next)
+
             async def _aio_sub():
                 try:
                     while True:
@@ -30,6 +33,11 @@ class EventBus:
                     loop.call_soon(functools.partial(observer.on_error, exception))
 
             task = asyncio.ensure_future(_aio_sub(), loop=loop)
-            return Disposable(task.cancel)
 
-        return rx.create(on_subscribe)
+            def on_subscriber_disposed():
+                task.cancel()
+            return Disposable(on_subscriber_disposed)
+
+        subject = Subject()
+        rx.create(on_subscribe).subscribe(subject)
+        return subject
