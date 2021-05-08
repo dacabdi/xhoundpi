4089472f45903069cf296f8d4726492dff70616d
David Cabrera <dacabdi@gmail.com>
2021-05-07 19:33:55 -0700

    prepare for eloy

--- test/test_unit_coordinate_offset.py
+++ test/test_unit_coordinate_offset.py
@@ -5,21 +5,61 @@
 # pylint: disable=invalid-name
 
 import unittest
-import unittest.mock
-from decimal import Decimal
+from unittest.mock import Mock
+from dataclasses import dataclass
+from ddt import ddt, data, unpack
 
-from xhoundpi.coordinate_offset import CoordinateOffset, StaticOffsetProvider
+from decimal import Decimal as D
+
+from xhoundpi.coordinate_offset import (
+    CoordinateOffset,
+    OrientationOffsetProvider,
+    EulerAngles,
+    StaticOffsetProvider,
+    StaticOrientationProvider,)
+
+class test_StaticOrientationProvider(unittest.TestCase):
+
+    def test_provide(self):
+        angles = EulerAngles(x=D("-1.5"), y=D("0.2"))
+        provider = StaticOrientationProvider(angles)
+
+        self.assertEqual(EulerAngles(x=D("-1.5"), y=D("0.2")),
+            provider.get_orientation(), msg = 'Because it must provide the preset angles')
+
+        self.assertEqual(EulerAngles(x=D("-1.5"), y=D("0.2")),
+            provider.get_orientation(), msg = 'Because it must provide the same value across calls')
 
 class test_StaticOffsetProvider(unittest.TestCase):
 
     def test_provide(self):
-        offset = CoordinateOffset(lat=Decimal("0.5"), lon=Decimal("-0.1"))
+        offset = CoordinateOffset(lat=D("0.5"), lon=D("-0.1"))
         provider = StaticOffsetProvider(offset)
 
         self.assertEqual(
-            CoordinateOffset(lat=Decimal("0.5"), lon=Decimal("-0.1")),
+            CoordinateOffset(lat=D("0.5"), lon=D("-0.1")),
             provider.get_offset(), msg = 'Because it must provide the preset offset')
 
         self.assertEqual(
-            CoordinateOffset(lat=Decimal("0.5"), lon=Decimal("-0.1")),
+            CoordinateOffset(lat=D("0.5"), lon=D("-0.1")),
             provider.get_offset(), msg = 'Because it must provide the same value across calls')
+
+
+def _data(x: D, y: D, z: D, r: D, lat: D, lon: D):
+    return (EulerAngles(x, y, z), r, CoordinateOffset(lat, lon))
+
+@ddt
+class test_OrientationOffsetProvider(unittest.TestCase):
+
+    @data(
+        _data(x=D("0.5"), y=D("0.5"), z=D("0.4"), r=D("10"), lat=D("11"), lon=D("10")),
+        _data(x=D("0.5"), y=D("0.5"), z=D("0.4"), r=D("10"), lat=D("11"), lon=D("10")),
+        _data(x=D("0.5"), y=D("0.5"), z=D("0.4"), r=D("10"), lat=D("11"), lon=D("10"))
+    )
+    @unpack
+    def test_provide(self, angles, radius, offset):
+        orientation_provider = Mock()
+        orientation_provider.get_orientation = Mock(return_value=angles)
+        provider = OrientationOffsetProvider(orientation_provider, radius)
+        self.assertEqual(offset, provider.get_offset())
+        orientation_provider.get_orientation.assert_called_once()
--- xhoundpi/coordinate_offset.py
+++ xhoundpi/coordinate_offset.py
@@ -8,6 +8,39 @@ from dataclasses import dataclass
 from decimal import Decimal
 
 @dataclass
+class EulerAngles:
+    '''
+    Model for Euler angles orientation
+    '''
+    # pylint: disable=invalid-name
+    x: Decimal = Decimal("0")
+    y: Decimal = Decimal("0")
+    z: Decimal = Decimal("0")
+
+
+class IOrientationProvider(ABC):
+    '''
+    Orientation provider in Euler angles
+    '''
+
+    @abstractmethod
+    def get_orientation(self) -> EulerAngles:
+        '''
+        Returns the orientation in euler angles
+        '''
+
+class StaticOrientationProvider(IOrientationProvider):
+    '''
+    Static orientation provider in Euler angles
+    '''
+
+    def __init__(self, angles: EulerAngles):
+        self.__angles = angles
+
+    def get_orientation(self) -> EulerAngles:
+        return self.__angles
+
+@dataclass
 class CoordinateOffset:
     '''
     Model for geographic coordinates offsets
@@ -37,3 +70,21 @@ class StaticOffsetProvider(ICoordinateOffsetProvider):
 
     def get_offset(self) -> CoordinateOffset:
         return self.__offset
+
+
+class OrientationOffsetProvider(ICoordinateOffsetProvider):
+    '''
+    Geographic coordinates dynamic offset provider
+    based off euler angles orientation and radius
+    '''
+
+    def __init__(self, orientation: IOrientationProvider, radius: Decimal):
+        self.__orientation = orientation
+        self.__radius = radius
+
+    def get_offset(self) -> CoordinateOffset:
+        angles = self.__orientation.get_orientation()
+        radius = self.__radius
+        return CoordinateOffset(
+            lat=angles.x + radius,
+            lon=angles.y + radius)
