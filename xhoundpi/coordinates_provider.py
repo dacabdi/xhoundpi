0fd1c663efd6eeacac20412c53eb8ad7d667c10c
David Cabrera <dacabdi@gmail.com>
2021-06-04 22:31:30 -0700

    create and setup current location provider

--- xhoundpi/coordinates_provider.py
+++ xhoundpi/coordinates_provider.py
@@ -4,6 +4,7 @@ Location provider
 
 from abc import ABC, abstractmethod
 
+from .dmath import DECIMAL0 as D0
 from .coordinates import GeoCoordinates
 
 class ICoordinatesProvider(ABC):
@@ -27,3 +28,20 @@ class StaticCoordinatesProvider(ICoordinatesProvider):
 
     def get_coordinates(self) -> GeoCoordinates:
         return self.__location
+
+class DynamicCoordinatesProvider(ICoordinatesProvider):
+    '''
+    Updatable coordinates provider
+    '''
+
+    def __init__(self):
+        self.__coordinates = GeoCoordinates(D0, D0, D0)
+
+    def get_coordinates(self) -> GeoCoordinates:
+        return self.__coordinates
+
+    def update(self, coordinates: GeoCoordinates) -> None:
+        '''
+        Set coordinates
+        '''
+        self.__coordinates = coordinates
